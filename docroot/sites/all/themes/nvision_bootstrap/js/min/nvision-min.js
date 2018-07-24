6c529580a8abe9dc1753c30345480b6e1f285e35
Gideon Cresswell <gideoncresswell@users.noreply.github.com>
2018-07-24 10:36:01 +0100

    By Gideon Cresswell: Added autoheight js

---
+++ docroot/sites/all/themes/nvision_bootstrap/js/min/nvision-min.js
@@ -0,0 +1,1 @@
+!function($){"use strict";$("body.site-status-message #hero-carousel").css({"padding-top":$("#site-status").height()})}(jQuery);
---
+++ docroot/sites/all/themes/nvision_bootstrap/js/nvision.js
@@ -0,0 +1,6 @@
+(function($) {
+  'use strict';
+
+  $("body.site-status-message #hero-carousel").css({"padding-top": $("#site-status").height() });
+
+})(jQuery);
