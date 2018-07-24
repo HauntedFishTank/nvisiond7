1cf76a7cc563f7f77283a9ab4832939a9039456f
Gideon Cresswell <gideoncresswell@users.noreply.github.com>
2018-07-24 11:30:21 +0100

    jkasfhkfj

--- docroot/sites/all/themes/nvision_bootstrap/js/nvision.js
+++ docroot/sites/all/themes/nvision_bootstrap/js/nvision.js
@@ -2,12 +2,12 @@
   Drupal.behaviors.nvision = {
     attach: function (context, settings) {
       // Run First
-      var newHeight = ($('#navbar').height() - 62);
+      var newHeight = ($('#navbar').height() - 52);
       $('body.site-status-message #hero-carousel').css({"padding-top": newHeight + 'px'});
 
       // Run on resize
       $(window).resize(function() {
-        var newHeight = ($('#navbar').height() - 62);
+        var newHeight = ($('#navbar').height() - 52);
         $('body.site-status-message #hero-carousel').css({"padding-top": newHeight + 'px'});
       });
 
